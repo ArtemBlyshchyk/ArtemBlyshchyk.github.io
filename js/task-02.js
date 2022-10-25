87069c7512ae8d58f38deb7589a495cb5c212319
ArtemBlyshchyk <88717285+ArtemBlyshchyk@users.noreply.github.com>
2022-10-25 23:51:41 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,14 +7,54 @@ const ingredients = [
   "Condiments",
 ];
 
+// const list = document.querySelector("#ingredients");
+
+// const ingredientsList = ingredients.forEach((ingredient) => {
+//   let listItem = document.createElement("li");
+//   listItem.classList.add("item");
+//   listItem.textContent = ingredient;
+//   list.appendChild(listItem);
+// });
+// ЯК ВАРІАНТ МОЖНА ДОБАВИТИ ЧЕРЕЗ ОДНУ ОПЕРАЦІЮ ЗА ДОПОМОГОЮ new DocumentFragment()
+// const list = document.querySelector("#ingredients");
+// let newPartOfCode = new DocumentFragment();
+
+// const ingredientsList = ingredients.forEach((ingredient) => {
+//   let listItem = document.createElement("li");
+//   listItem.classList.add("item");
+//   listItem.innerHTML = ingredient;
+//   newPartOfCode.appendChild(listItem);
+// });
+// let ul = list.appendChild(newPartOfCode);
+// console.log(list.appendChild(newPartOfCode));
+
+
 const list = document.querySelector("#ingredients");
 
-const ingredientsList = ingredients.forEach((ingredient) => {
-  let listItem = document.createElement("li");
-  listItem.classList.add("item");
-  listItem.textContent = ingredient;
-  list.appendChild(listItem);
-});
+// const ingredientsList = ingredients.map((ingredient) => {
+//   let listItem = document.createElement("li");
+//   listItem.classList.add("item");
+//   listItem.textContent = ingredient;
+//   return listItem;
+// });
+// console.log(ingredientsList);
+
+// list.append(...ingredientsList);
+
+// Зробимо через функцію
+const listIngredients = ingredient => {
+  return ingredients.map((ingredient) => {
+    let listItem = document.createElement("li");
+    listItem.classList.add("item");
+    listItem.textContent = ingredient;
+    return listItem;
+  });
+}
+
+  const ingredientsList = listIngredients(ingredients);
+  console.log(ingredientsList);
+
+  list.append(...ingredientsList);
 
 // Завдання 2
 // HTML містить порожній список ul#ingredients.
